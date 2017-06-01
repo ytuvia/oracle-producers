5e27e7597f2e897f4ab4b54dba4856bb65594e4e
Yehuda Tuvia <ytuvia@gmail.com>
2017-06-01 15:39:57 +0300

    config

--- app/config.ts
+++ app/config.ts
@@ -1,9 +1,31 @@
+import { fs } from 'fs';
+
 public class Config {
-  file: string;
-  constructor(file: string){
-    
-    return this.file;
+  json: string;
+  constructor(filename: string): Promise<any>{
+    return new Promise((resolve, reject)=>{
+      filename = filename || './app/config.json';
+      fs.readFile(filename,(err, result) => {
+          if (err) reject(err);
+          else {
+            let config = JSON.parse(result);
+            config.twitter = {
+              consumer_key: process.env.TW_CONSUMER_KEY,
+              consumer_secret: process.env.TW_CONSUMER_SECRET,
+              access_token_key: process.env.TW_ACCESS_TOKEN_KEY,
+              access_token_secret: process.env.TW_ACCESS_TOKEN_SECRET 
+            }
+            this.json = config;
+            resolve(config);
+          }
+      });
+    })
   };
 }
 
-export var config = new Config();
+async function init(filename: string){
+  let config = await new Config(filename);
+  return config.json;
+}
+
+export var config = init;
